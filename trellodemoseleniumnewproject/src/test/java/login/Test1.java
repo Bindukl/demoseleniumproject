29fa3e03ac8f0d2f0b2364752e232c1618fd4cc9
Lingaraj <lingarajdas75@gmail.com>
2024-07-11 12:18:43 +0530

    Done I got it

--- trellodemoseleniumnewproject/src/test/java/login/Test1.java
+++ trellodemoseleniumnewproject/src/test/java/login/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
  public static void main(String[] args) {
 	System.out.println("Atomation Testing script");
 	System.out.println("Atomation Testing script successfully lunched for first time");
+	System.out.println("Done I got it");
 }
 }
