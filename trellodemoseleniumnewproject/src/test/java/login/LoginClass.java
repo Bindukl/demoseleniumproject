5a21c87605e957a6e48d3548591cf5c360016568
Bindukl <157616300+Bindukl@users.noreply.github.com>
2024-07-11 11:50:55 +0530

    Update LoginClass.java

--- trellodemoseleniumnewproject/src/test/java/login/LoginClass.java
+++ trellodemoseleniumnewproject/src/test/java/login/LoginClass.java
@@ -4,7 +4,8 @@ public class LoginClass {
 	
 	public static void main(String[] args) {
 		System.out.println("login into application  successfully");
-		System.out.println("login into application  successfully");
+		System.out.println("login into application  2nd time successfully");
+		System.out.println("login into application  3rd time successfully");
 	}
 
 }
